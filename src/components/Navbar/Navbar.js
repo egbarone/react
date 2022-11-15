8d7cdba977723a9e6526da338cd8b81e7a452bae
egbarone <106161938+egbarone@users.noreply.github.com>
2022-11-15 16:20:24 -0300

    agregado carpeta components/Navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+         <nav>
+            <h1 className='h1' >Cerveceria One</h1>
+            <button className='botones' >Cervezas</button>
+            <button className='botones' >Pedidos</button>
+            <button className='botones' >Nosotros</button>
+         </nav>
+    )
+}
+
+export default Navbar 
